731003178d991bfe8b1223966fd64b8e5d5c5642
ArnavNigam09 <83456570+ArnavNigam09@users.noreply.github.com>
2021-11-03 13:53:17 +0530

    Add files via upload
    Correction, had to change width and height parameter from 'block_image_object' to below values:
    block_image_object.scaleToWidth(690);
    block_image_object.scaleToHeight(480);

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ function new_image(get_image)
 	fabric.Image.fromURL(get_image, function(Img) {
 	block_image_object = Img;
 
-	block_image_object.scaleToWidth(block_image_object);
-	block_image_object.scaleToHeight(block_image_object);
+	block_image_object.scaleToWidth(690);
+	block_image_object.scaleToHeight(480);
 	block_image_object.set({
 
 		top:block_y,
